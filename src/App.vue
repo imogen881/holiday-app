3284f243c5ea9d0094ed31bff171d54ae646f1db
Imogen <IDavey@qa.com>
2021-06-15 09:20:51 +0100

    initial layout

---
+++ src/App.vue
@@ -0,0 +1,118 @@
+<template>
+  <div id="app">
+    <h1 class="header">HOLIDAY</h1>
+
+    <h2>Available packages</h2>
+
+    <div class="p-grid p-ai-stretch vertical-container">
+      <div class="p-col">
+        <Card
+          id="card"
+          style="width: 25rem; margin-bottom: 2em"
+          v-for="holiday in holidays"
+          v-bind:key="holiday.id"
+        >
+          <template #header>
+            <img v-bind:src="getImage(holiday)" />
+          </template>
+          <template #title>
+            {{ holiday.name }}
+          </template>
+          <template #content>
+            {{ holiday.destination }}
+            {{ holiday.country }}
+            {{ holiday.priceGBP }}
+          </template>
+          <template #footer>
+            <Button icon="pi pi-check" label="Book" />
+          </template>
+        </Card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  methods: {
+    getImage(holiday) {
+      return require("./assets/" + holiday.image + ".jpg");
+    },
+  },
+  data() {
+    return {
+      holidays: [
+        {
+          id: 1,
+          name: "Two weeks in the Canaries!",
+          image: "tenerife",
+          destination: "Tenerife",
+          country: "Spain",
+          priceGBP: "500",
+        },
+        {
+          id: 2,
+          name: "City-break in NYC!",
+          image: "ny",
+          destination: "New York City",
+          country: "United States of America",
+          priceGBP: "1200",
+        },
+        {
+          id: 3,
+          name: "All inclusive in Greece!",
+          image: "crete",
+          destination: "Crete",
+          country: "Greece",
+          priceGBP: "800",
+        },
+        {
+          id: 4,
+          name: "All inclusive in Greece!",
+          image: "crete",
+          destination: "Crete",
+          country: "Greece",
+          priceGBP: "800",
+        },
+        {
+          id: 5,
+          name: "All inclusive in Greece!",
+          image: "crete",
+          destination: "Crete",
+          country: "Greece",
+          priceGBP: "800",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  font-family: "montserrat", sans-serif;
+}
+
+.header {
+  padding: 10px 0px;
+}
+
+p {
+  line-height: 1.5;
+  margin: 0;
+}
+
+#card {
+  margin: 10px;
+  display: inline-block;
+}
+
+img {
+  float: left;
+  height: 250px;
+  object-fit: cover;
+  margin-bottom: 15px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,24 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import Card from 'primevue/card';
+import Menubar from 'primevue/menubar';
+
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
+const app = createApp(App)
+
+app.use(PrimeVue)
+
+app.component('Button', Button);
+app.component('InputText', InputText)
+app.component('Card', Card)
+app.component('Menubar', Menubar)
+
+// const app = createApp(App);
+app.mount('#app');
